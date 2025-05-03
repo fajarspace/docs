36928e7f893133ad50fb34a9709f85cf6d9fc9d9
fajarspace <fajar.agngn@gmail.com>
2025-05-03 15:44:02 +0700

    added special char

--- tina/config.js
+++ tina/config.js
@@ -11,12 +11,12 @@ export default defineConfig({
 
   build: {
     outputFolder: "admin",
-    publicFolder: "static", // Changed from "public" to "static" for Hugo
+    publicFolder: "out", // Changed from "public" to "static" for Hugo
   },
   media: {
     tina: {
       mediaRoot: "images", // Added a specific media folder
-      publicFolder: "static", // Changed from "public" to "static" for Hugo
+      publicFolder: "out", // Changed from "public" to "static" for Hugo
     },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
